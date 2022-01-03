0ecc209cf1fcbcc3e4fee4bd76005cfc7a66ed76
seyeong <seyoung7555@naver.com>
2022-01-03 17:48:26 +0900

    이진수를 10진수로 변환하는 함수 구현 완료

--- src/BinaryAdder/Convertor.java
+++ src/BinaryAdder/Convertor.java
@@ -8,23 +8,45 @@ public class Convertor {
 
     public void dec2bin(int decimal) {
         List<Boolean> answer = new ArrayList<Boolean>();
-        while (decimal > 0) {
-            boolean b = (decimal % 2) != 0;
+        int dec = decimal;
+        while (dec > 0) {
+            boolean b = (dec % 2) != 0;
             answer.add(b);
-            decimal /= 2;
+            dec /= 2;
         }
         Boolean[] arr = new Boolean[answer.size()];
-        printBin(answer.toArray(arr));
+        printBin(decimal, answer.toArray(arr));
     }
 
-    public void printBin(Boolean[] arr) {
-        System.out.println(Arrays.toString(arr));
+    public void bin2dec(boolean[] bin) {
+        int i = 1, answer = 0;
+        for (boolean b : bin) {
+            if (b) {
+                answer += i;
+            }
+
+            i *= 2;
+        }
+        printDec(bin, answer);
     }
+
+    public void printBin(int decimal, Boolean[] answer) {
+        System.out.println("입력 = "+decimal);
+        System.out.println("출력 = "+Arrays.toString(answer));
+    }
+
+    public void printDec(boolean[] bin, int answer) {
+        System.out.println("입력 = "+Arrays.toString(bin));
+        System.out.println("출력 = "+answer);
+    }
+
 
     public static void main(String[] args) {
         Convertor c = new Convertor();
         int decimal = 256;
         c.dec2bin(decimal);
+        boolean[] bin = {true, true, true, true, true};
+        c.bin2dec(bin);
 
     }
 }
