787ecbe6da4ddca2b03884d3277d62489ac0a086
seyeong <seyoung7555@naver.com>
2022-01-03 17:05:39 +0900

    1바이트 덧셈기 구현 완료
    - 길이가 다르거나 8비트 이상도 계산 가능하도록 구현하려고 하였음

--- src/BinaryAdder/BinaryAdder.java
+++ src/BinaryAdder/BinaryAdder.java
@@ -16,10 +16,55 @@ public class BinaryAdder {
         return answer;
     }
 
+    public boolean[] byteAdder(boolean[] byteA, boolean[] byteB) {
+        int min_len = Math.min(byteA.length, byteB.length);
+        int max_len = Math.max(byteA.length, byteB.length);
+        boolean carry = false;
+
+        int answer_len = max_len+1;
+
+        if (min_len != max_len) {
+            answer_len = max_len;
+        }
+
+        boolean[] answer = new boolean[answer_len];
+
+        for (int i = 0 ; i < min_len ; i++) {
+            boolean bitA=byteA[i], bitB=byteB[i];
+            boolean[] result = this.fullAdder(bitA, bitB, carry);
+            carry = result[0];
+            answer[i] = result[1];
+        }
+
+        if (max_len != min_len) {
+            for (int j = min_len; j < max_len; j++) {
+                if (byteA.length > byteB.length) {
+                    boolean bitA = byteA[j], bitB = false;
+                    boolean[] result = this.fullAdder(bitA, bitB, carry);
+                    carry = result[0];
+                    answer[j] = result[1];
+                } else {
+                    boolean bitA = false, bitB = byteB[j];
+                    boolean[] result = this.fullAdder(bitA, bitB, carry);
+                    carry = result[0];
+                    answer[j] = result[1];
+                }
+            }
+        } else
+        {
+            answer[max_len] = carry;
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
 
         BinaryAdder binaryAdder = new BinaryAdder();
-        boolean[] answer = binaryAdder.fullAdder(false, false, true);
+        boolean[] byteA = {true, true, false, true, true, false, true, false};
+        boolean[] byteB = {true, false, true, true, false, false, true, true};
+
+        boolean[] answer = binaryAdder.byteAdder(byteA, byteB);
 
         for (boolean a : answer) {
             System.out.println(a);
